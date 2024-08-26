0f0bc8a8741d4a127414e2a327423e866c61ba96
Manu Sharma <msharma23225@gmail.com>
2024-08-26 17:39:16 +0530

    Update ques3.java

--- ultimate/ques3.java
+++ ultimate/ques3.java
@@ -42,6 +42,18 @@ public static boolean isPalindrome(Node head) {
     }
     return true;
 }
+    public static void reversee(){
+    Node prev = null;
+    Node curr = tail = head ;
+    Node next;
+    while(curr != null){
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    head = prev;
+}
 
 
 public static void main(String[] args) {
