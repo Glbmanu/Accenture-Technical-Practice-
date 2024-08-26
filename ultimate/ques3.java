fe872f31d5a0b712e462a45e3e45b3d6b164bb63
Manu <msharma23225@gmail.com>
2024-08-26 17:20:47 +0530

    Accenture Technical Practice

--- ultimate/ques3.java
+++ ultimate/ques3.java
@@ -1,3 +1,58 @@
+
+// Linked List Palindrome
+
 package ultimate;
-public class ques3 {
+import java.util.Scanner;
+import java.util.Stack;
+public class ques3{
+class Node {
+    int data;
+    Node next;
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
 }
+static Node head;
+static Node tail;
+public void  addlast(int data){
+    Node newNode = new Node(data);
+    if (head == null){
+        head = tail = newNode;
+        return;
+    }
+    tail.next = newNode;
+    tail = newNode;
+
+}
+public static boolean isPalindrome(Node head) {
+    Stack<Integer> st = new Stack<>();
+    Node temp = head;
+    while (temp != null) {
+        st.push(temp.data);
+        temp = temp.next;
+    }
+    temp = head;
+    while (temp != null) {
+        if (temp.data != st.peek()) {
+            return false;
+        }
+        st.pop();
+        temp = temp.next;
+    }
+    return true;
+}
+
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    ques3 ll = new ques3();
+    int n = sc.nextInt();
+    for (int i = 0; i < n; i++) {
+        int a = sc.nextInt();
+        ll.addlast(a);
+    }
+    System.out.println(isPalindrome(head));
+}
+}
+
